94e5d818c91d76f9b1f6f55cdf200096004794a6
dustin-biser <dustinbiser@gmail.com>
2016-07-05 01:29:56 -0400

    Added NumCubes and CubeRandomness Sliders

--- Source/ViewController.mm
+++ Source/ViewController.mm
@@ -12,18 +12,29 @@
 
 @property (strong, nonatomic) EAGLContext * eaglContext;
 
+- (void) setupSliderForNumCubes;
+
+- (void) setupSliderForCubeRandomness;
+
+- (void)sliderActionNumCubes:(id)sender forEvent:(UIEvent*)event;
+
 @end
 
 
 @implementation ViewController {
 @private
     GLKView * _glkView;
+    
+    // Slider UI Controls
+    UISlider * _slider_numCubes;
+    UISlider * _slider_cubeRandomness;
+    
     CubenadoRenderer * _cubenadoRenderer;
 }
 
 
 //---------------------------------------------------------------------------------------
-- (void)viewDidLoad
+- (void) viewDidLoad
 {
     [super viewDidLoad];
     
@@ -63,8 +74,109 @@
     framebufferSize.height = static_cast<GLint>(frameSize.height);
     
     _cubenadoRenderer = [[CubenadoRenderer alloc] initWithFramebufferSize:framebufferSize];
+    
+    
+    [self setupSliderForNumCubes];
+    
+    [self setupSliderForCubeRandomness];
 }
 
+
+//---------------------------------------------------------------------------------------
+- (void) setupSliderForNumCubes
+{
+    CGRect frame = self.view.frame;
+    _slider_numCubes = [[UISlider alloc] initWithFrame:frame];
+    [self.view addSubview:_slider_numCubes];
+    
+    [_slider_numCubes addTarget:self
+                         action:@selector(sliderActionNumCubes:forEvent:)
+               forControlEvents:UIControlEventValueChanged];
+    
+    [_slider_numCubes setBackgroundColor:[UIColor clearColor]];
+    _slider_numCubes.minimumValue = 0.0;
+    _slider_numCubes.maximumValue = 1.0;
+    _slider_numCubes.continuous = YES;
+    _slider_numCubes.value = 0.5;
+    
+    _slider_numCubes.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    auto viewMargins = self.view.layoutMarginsGuide;
+    
+    // Pin the leading edge of slider to viewMargin's leading edge
+    [_slider_numCubes.leadingAnchor constraintEqualToAnchor:viewMargins.leadingAnchor].active = YES;
+    
+    // Pin the trailing edge of slider to viewMargin's trailing edge
+    [_slider_numCubes.trailingAnchor constraintEqualToAnchor:viewMargins.trailingAnchor].active = YES;
+    
+    // Offset slider's bottom anchor by 15% from parent view's bottom anchor.
+    CGFloat frameHeight = self.view.frame.size.height;
+    CGFloat constantValue = -(frameHeight * 0.15f);
+    [_slider_numCubes.bottomAnchor constraintEqualToAnchor:viewMargins.bottomAnchor
+                                                  constant:constantValue].active = YES;
+}
+
+
+//---------------------------------------------------------------------------------------
+- (void) setupSliderForCubeRandomness
+{
+    CGRect frame = self.view.frame;
+    _slider_cubeRandomness = [[UISlider alloc] initWithFrame:frame];
+    [self.view addSubview:_slider_cubeRandomness];
+    
+    [_slider_cubeRandomness addTarget:self
+                         action:@selector(sliderActionCubeRandomness:forEvent:)
+               forControlEvents:UIControlEventValueChanged];
+    
+    [_slider_cubeRandomness setBackgroundColor:[UIColor clearColor]];
+    _slider_cubeRandomness.minimumValue = 0.0;
+    _slider_cubeRandomness.maximumValue = 1.0;
+    _slider_cubeRandomness.continuous = YES;
+    _slider_cubeRandomness.value = 0.0;
+    
+    _slider_cubeRandomness.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    auto viewMargins = self.view.layoutMarginsGuide;
+    
+    // Pin the leading edge of slider to viewMargin's leading edge
+    {
+        auto anchor = viewMargins.leadingAnchor;
+        [_slider_cubeRandomness.leadingAnchor constraintEqualToAnchor:anchor].active = YES;
+    }
+    
+    // Pin the trailing edge of slider to viewMargin's trailing edge
+    {
+        auto anchor = viewMargins.trailingAnchor;
+        [_slider_cubeRandomness.trailingAnchor constraintEqualToAnchor:anchor].active = YES;
+    }
+    
+    // Offset slider's bottom anchor by 5% from parent view's bottom anchor.
+    {
+        CGFloat frameHeight = self.view.frame.size.height;
+        CGFloat constantValue = -(frameHeight * 0.05f);
+        auto anchor = viewMargins.bottomAnchor;
+        [_slider_cubeRandomness.bottomAnchor constraintEqualToAnchor:anchor
+                                                            constant:constantValue].active = YES;
+    }
+}
+
+
+//---------------------------------------------------------------------------------------
+- (void)sliderActionNumCubes:(id)sender forEvent:(UIEvent*)event
+{
+    if ([sender isMemberOfClass:[UISlider class]])  {
+        NSLog(@"Slider-NumCubes: %f", _slider_numCubes.value);
+    }
+}
+
+
+//---------------------------------------------------------------------------------------
+- (void)sliderActionCubeRandomness:(id)sender forEvent:(UIEvent*)event
+{
+    if ([sender isMemberOfClass:[UISlider class]])  {
+        NSLog(@"Slider-CubeRandomness: %f", _slider_cubeRandomness.value);
+    }
+}
 
 //---------------------------------------------------------------------------------------
 // GLKViewController update method
